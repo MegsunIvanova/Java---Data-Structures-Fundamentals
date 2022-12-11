3bf09a887c58f95a427f6375f3f5aa633ae44d09
MegsunIvanova <megsunmeg@gmail.com>
2022-12-11 23:16:38 +0200

    src from training theme_04_Heaps_BST Lab

---
+++ theme_04_Heaps_BST/Lab/src/main/java/implementations/BinarySearchTree.java
@@ -0,0 +1,121 @@
+package implementations;
+
+import interfaces.AbstractBinarySearchTree;
+
+public class BinarySearchTree<E extends Comparable<E>> implements AbstractBinarySearchTree<E> {
+
+    private Node<E> root;
+    private Node<E> leftChild;
+    private Node<E> rightChild;
+
+    public BinarySearchTree() {
+    }
+
+    public BinarySearchTree(Node<E> root) {
+        this.copy(root);
+
+    }
+
+    private void copy(Node<E> node) {
+        if (node != null) {
+            this.insert(node.value);
+            this.copy(node.leftChild);
+            this.copy(node.rightChild);
+        }
+    }
+
+    @Override
+    public void insert(E element) {
+        Node<E> newNode = new Node<>(element);
+        if (this.getRoot() == null) {
+            this.root = newNode;
+        } else {
+            Node<E> current = this.root;
+            Node<E> previous = current;
+
+            while (current != null) {
+                previous = current;
+                if (isLess(element, current.value)) {
+                    current = current.leftChild;
+                } else if (isGrater(element, current.value)) {
+                    current = current.rightChild;
+                } else if (areEqual(element, current.value)) {
+                    return;
+                }
+
+            }
+
+            if (isLess(element, previous.value)) {
+                previous.leftChild = newNode;
+            } else if (isGrater(element, previous.value)) {
+                previous.rightChild = newNode;
+            }
+        }
+    }
+
+    @Override
+    public boolean contains(E element) {
+        Node<E> current = this.root;
+
+        while (current != null) {
+            if (isLess(element, current.value)) {
+                current = current.leftChild;
+            } else if (isGrater(element, current.value)) {
+                current = current.rightChild;
+            } else if (areEqual(element, current.value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public AbstractBinarySearchTree<E> search(E element) {
+        AbstractBinarySearchTree<E> result = new BinarySearchTree<E>();
+
+        Node<E> current = this.root;
+
+        while (current != null) {
+            if (isLess(element, current.value)) {
+                current = current.leftChild;
+            } else if (isGrater(element, current.value)) {
+                current = current.rightChild;
+            } else if (areEqual(element, current.value)) {
+                return new BinarySearchTree<>(current);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Node<E> getRoot() {
+        return this.root;
+    }
+
+    @Override
+    public Node<E> getLeft() {
+        return this.leftChild;
+    }
+
+    @Override
+    public Node<E> getRight() {
+        return this.rightChild;
+    }
+
+    @Override
+    public E getValue() {
+        return this.root.value;
+    }
+
+    private boolean isLess(E first, E second) {
+        return first.compareTo(second) < 0;
+    }
+
+    private boolean isGrater(E first, E second) {
+        return first.compareTo(second) > 0;
+    }
+
+    private boolean areEqual(E first, E second) {
+        return first.compareTo(second) == 0;
+    }
+}
